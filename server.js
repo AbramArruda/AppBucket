e901c38137d865f453aa61e28acc47406150783b
Abram <abramarruda@gmail.com>
2019-02-06 15:35:25 -0500

    added serverside validation and client side validation, as well as routes to adding a idea

--- server.js
+++ server.js
@@ -5,10 +5,19 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+//MAP global promise -- Getting rid of deprication warning
+mongoose.Promse = global.Promise;
 //Connect to mongoose
-mongoose.connect("mongodb://localhost/appbucket-dev", {
-  useMongoClient: true
-});
+mongoose
+  .connect("mongodb://localhost/appbucket-dev", {
+    useNewUrlParser: true
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+//Loade Idea Model
+require("./models/Idea");
+const Idea = mongoose.model("ideas");
 
 //express-handlebars middleware
 //tells the system that we want to use the handlebars template engine and set the default layout to main
@@ -23,7 +32,32 @@ app.get("/", function(req, res) {
 app.get("/about", function(req, res) {
   res.render("about");
 });
+//Add idea form route
+app.get("/ideas/add", function(req, res) {
+  res.render("ideas/add");
+});
 
+//Process form
+app.post("/ideas", function(req, res) {
+  let errors = [];
+
+  if (!req.body.title) {
+    errors.push({ text: "Please add a title" });
+  }
+  if (!req.body.details) {
+    errors.push({ text: "Please add some details" });
+  }
+
+  if (errors.length > 0) {
+    res.render("ideas/add", {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    res.send("passed");
+  }
+});
 const PORT = 3000;
 app.listen(PORT, function() {
   console.log(`Server listening on Port ${PORT}`);
